aa7f5641dc3e292198a5b3989d1d030ab72cc5ca
ArunArangan <arundark48@gmail.com>
2022-06-30 22:13:44 +0530

    Create express and mongo connectivity

---
+++ index.js
@@ -0,0 +1,30 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import { userRouter } from "./routes/users.js";
+
+dotenv.config();
+
+const app = express();
+const port = 5000;
+const URL = process.env.MONGO_URL;
+
+app.use(express.json());
+app.use("/user", userRouter);
+
+mongoose
+  .connect(URL, {
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log("Connected DB Secussesfully");
+  })
+  .catch((err) => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("Hello world!");
+});
+
+app.listen(port, () => {
+  console.log("server listening on port " + port);
+});
