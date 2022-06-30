b1b294777a292780d54f2782f77bf62906a97784
ArunArangan <arundark48@gmail.com>
2022-06-30 22:15:15 +0530

    created pin schema

---
+++ models/pin.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+const pinSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+      min: 3,
+      max: 50,
+    },
+    desc: {
+      type: String,
+      required: true,
+      min: 3,
+      max: 100,
+    },
+    rating: {
+      type: Number,
+      required: true,
+      min: 0,
+      max: 5,
+    },
+    long: {
+      type: Number,
+      required: true,
+    },
+    lat: {
+      type: Number,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+// module.exports = mongoose.model("pin", pinSchema);
+export const pin = pinSchema;
